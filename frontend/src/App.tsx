0eb4f669b6a7109dbf4aa6fab1b1298aebb82da1
“Adolfo <ado.medina.val2094@gmail.com>
2026-04-24 15:03:59 -0400

    feat(routing): agregar redirects de URLs viejas del front anterior a /mi-perfil

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -29,10 +29,12 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
+            {/* Públicas */}
             <Route path="/" element={<Index />} />
             <Route path="/login" element={<Login />} />
             <Route path="/registro" element={<Registro />} />
 
+            {/* Requieren sesión */}
             <Route
               path="/buscar"
               element={
@@ -50,6 +52,12 @@ const App = () => (
               }
             />
 
+            {/* Redirects de URLs viejas del front anterior */}
+            <Route path="/perfil/trabajadora" element={<Navigate to="/mi-perfil" replace />} />
+            <Route path="/perfil/clienta" element={<Navigate to="/mi-perfil" replace />} />
+            <Route path="/mi-perfil-profesional" element={<Navigate to="/mi-perfil" replace />} />
+
+            {/* 404 */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
